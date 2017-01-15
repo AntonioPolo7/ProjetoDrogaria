417eafa8cac72ab1676cb6462902c3d8040fdfd2
AntonioPolo7 <acesarpolo@gmail.com>
2017-01-15 11:44:03 -0200

    ok

--- ProjetoDrogaria/src/br/com/drogaria/domain/Fabricante.java
+++ ProjetoDrogaria/src/br/com/drogaria/domain/Fabricante.java
@@ -2,4 +2,28 @@ package br.com.drogaria.domain;
 
 public class Fabricante {
 
+	private Long codigo; /* Long maiusculo */
+
+	private String descricao;
+
+	/* criar getters / setters */
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+
 }
